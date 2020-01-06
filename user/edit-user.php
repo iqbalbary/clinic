35ab70bc90c125b3cb13da227365a42d6a4d662e
nawaz+1-at-036055243820 <boo2.nawaz@gmail.com>
2020-01-06 07:34:51 +0600

    field added  edit profile

--- user/edit-user.php
+++ user/edit-user.php
@@ -19,6 +19,18 @@ require_once('../header.php');
         <label for="Phone">Phone Number</label>
         <input type="text" required="required" class="form-control" name="Phone"  placeholder="+8801763015332">
     </div>
+    <div class="form-group">
+        <label for="Father_Name">Father Name</label>
+        <input type="text" class="form-control"  required='true'  name="Father_Name" placeholder="Mr. X">
+    </div>
+    <div class="form-group">
+        <label for="Mother_Name">Mother Name</label>
+        <input type="text" class="form-control"  required='true'  name="Mother_Name" placeholder="Mis. Y">
+    </div>
+    <div class="form-group">
+        <label for="Mother_Name">Short Description</label>
+        <textarea  type="textarea" class="md-textarea form-control"  required='true'  name="Short_Description" placeholder="Short Description" rows="10"> </textarea>
+    </div>
     <button type="submit" class="btn btn-primary" name='submit'>Submit</button>
     </form>
 </div>
