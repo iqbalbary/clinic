6b40041b8d32b9d712963b9ee87ba528ba1b7ca6
NawazShorif <nawaz.cse.ruet@gmail.com>
2020-02-04 23:44:38 +0600

    user deposit table insert logic

--- deposit/verify-deposit.php
+++ deposit/verify-deposit.php
@@ -1,64 +1,60 @@
 <?php
-// Add verify  Deposit 
-require('../header.php');
+// Add verify  Deposit
+require '../header.php';
+require_once '../helper.php';
 
-if(!$isSession  ){
-    header("Location: ".$base_url."index.php");
+if (!$isSession) {
+    header("Location: " . $base_url . "index.php");
 }
 $userListData = dataFetchUsingTable("User", array('USER_ID', "Image", "Name"));
-
-$userListArr = array();
-foreach ($userListData as $data) {
-    $userListArr[$data['USER_ID']] = $data;
-}
-
+$userListArr = dataMapByUniqeField("USER_ID", $userListData);
 $depositId = "";
-if(isset($_GET['deposit-id'])){
-	$depositId = $_GET['deposit-id'];
-}
-else{
-	header("Location: ".$base_url."index.php");
+if (isset($_GET['deposit-id'])) {
+    $depositId = $_GET['deposit-id'];
+} else {
+    header("Location: " . $base_url . "index.php");
 }
 
 $userDepositCondintion[] = array(
-    'fieldName'=> 'Deposite_ID',
+    'fieldName' => 'Deposite_ID',
     'symbol' => " = ",
-    'value' => $depositId
+    'value' => $depositId,
 );
 
-
-$depositDetailsDataArr = dataFetchUsingTable("deposite", array() , $userDepositCondintion);
+$depositDetailsDataArr = dataFetchUsingTable("deposite", array(), $userDepositCondintion);
 $depositDetailsData = $depositDetailsDataArr[0];
-$selectedUserIds = explode(",",$depositDetailsData["Profile_ID"]);
-$selectedMonths = explode(",",$depositDetailsData["Month"]);
+$selectedUserIds = explode(",", $depositDetailsData["Profile_ID"]);
+$selectedMonths = explode(",", $depositDetailsData["Month"]);
 
-if(isset($_POST['Deposite_ID'])){
-	$userDepositArr = array(
-		"user_id" => "",
-		"month" => "",
-		"deposit_id"=> $depositId,
-		"amount" => 5000,
-		"late_fine" => 0
-	);
+if (isset($_POST['Deposite_ID'])) {
+    $userDepositArr = array(
+        "user_id" => "",
+        "month" => "",
+        "deposit_id" => $depositId,
+        "amount" => 0,
+        "late_fine" => 0,
+    );
 
-	foreach ($selectedUserIds as  $selectedUserId) {
-		$userDepositArr['user_id'] =  $selectedUserId;
-		foreach ($selectedMonths as  $selectedMonth) {
-			$userDepositArr['month'] =  $selectedMonth;
-			$insertFlag=  insetrtDataFunc("user_deposite", $userDepositArr);
-		}
-	}
+    foreach ($selectedUserIds as $selectedUserId) {
+        $userDepositArr['user_id'] = $selectedUserId;
+        foreach ($selectedMonths as $selectedMonth) {
+            $userDepositArr['month'] = $selectedMonth;
+            $userDepositArr['amount'] = $_POST["user-" . $selectedUserId . "-month-" . $selectedMonth . "-amount"];
+            $userDepositArr['late_fine'] = $_POST["user-" . $selectedUserId . "-month-" . $selectedMonth . "-late-fine"];
 
-
-	$UserDepositUpdateArray =array(
-		"Verification_ID" => $_SESSION["USER_ID"],
-		"Verification" => 1
-	);
-	$updateFlag = updateData("deposite", $UserDepositUpdateArray, $userDepositCondintion );
-
-    if($updateFlag){
-    	$depositDetailsData["Verification_ID"]  = $_SESSION["USER_ID"];
-    	$depositDetailsData["Verification"]  = 1;
+            if ($userDepositArr['amount'] || $userDepositArr['late_fine']) {
+                $insertFlag = insetrtDataFunc("user_deposite", $userDepositArr);
+            }
+        }
+    }
+    $UserDepositUpdateArray = array(
+        "Verification_ID" => $_SESSION["USER_ID"],
+        "Verification" => 1,
+    );
+    $updateFlag = updateData("deposite", $UserDepositUpdateArray, $userDepositCondintion);
+    if ($updateFlag) {
+        $depositDetailsData["Verification_ID"] = $_SESSION["USER_ID"];
+        $depositDetailsData["Verification"] = 1;
     }
 
 }
@@ -70,15 +66,15 @@ if(isset($_POST['Deposite_ID'])){
             <label for="Profile ID">Profile ID</label>
             <div class="profile-data-container">
                 <div class="custom-input">
-                    <?php 
-                        foreach ($selectedUserIds as $selectedUserId) { ?>
+                    <?php
+foreach ($selectedUserIds as $selectedUserId) {?>
                     <div class="input-item-block">
                         <img
-                            src="<?php echo $base_url .'uploads/User/Image/' . trim($userListArr[$selectedUserId]['Image'] ? $userListArr[$selectedUserId]['Image'] :  'avater.jpg')  ?>">
+                            src="<?php echo $base_url . 'uploads/User/Image/' . trim($userListArr[$selectedUserId]['Image'] ? $userListArr[$selectedUserId]['Image'] : 'avater.jpg') ?>">
                         <span> <?php echo $userListArr[$selectedUserId]['Name']; ?> </span>
 
                     </div>
-                    <?php } ?>
+                    <?php }?>
                 </div>
             </div>
         </div>
@@ -93,17 +89,17 @@ if(isset($_POST['Deposite_ID'])){
         <div class="form-group">
             <label for="Month">Month</label>
             <div class="custom-input">
-                <?php 
-                    foreach ($selectedMonths as $selectedMonth) { ?>
+                <?php
+foreach ($selectedMonths as $selectedMonth) {?>
                 <div class="input-item-block"> <?php echo $monthArray[$selectedMonth]; ?> </div>
-                <?php } ?>
+                <?php }?>
             </div>
         </div>
         <div class="form-group">
             <div class="image-label-block">
                 <label for="Deposite_Slip"> Deposite Slip </label>
                 <img
-                    src="<?php echo $base_url .'uploads/Deposite/Deposite_Slip/' . trim($depositDetailsData['Deposite_Slip'] ? $depositDetailsData['Deposite_Slip'] :  'avater.jpg')  ?>">
+                    src="<?php echo $base_url . 'uploads/Deposite/Deposite_Slip/' . trim($depositDetailsData['Deposite_Slip'] ? $depositDetailsData['Deposite_Slip'] : 'avater.jpg') ?>">
             </div>
 
         </div>
@@ -111,13 +107,35 @@ if(isset($_POST['Deposite_ID'])){
             <label for="Short_Description">Short Description</label>
             <p> <?php echo $depositDetailsData['Short_Description']; ?> </p>
         </div>
-        <?php if($depositDetailsData['Verification'] == 0){ ?>
-        <button type="submit" class="btn btn-primary" value="<?php echo $depositDetailsData['Deposite_ID'];  ?>"
+        <?php
+if ($depositDetailsData['Verification'] == 0) {
+    foreach ($selectedUserIds as $selectedUserId) {
+        foreach ($selectedMonths as $selectedMonth) {
+            ?>
+        <div class="user-month-name-image-container">
+            <img src="<?=$base_url?>uploads/User/Image/<?=$userListArr[$selectedUserId]['Image'] ? trim($userListArr[$selectedUserId]['Image']) : 'avater.jpg';?>"
+                alt="image">
+            <div class="user-name"><?=$userListArr[$selectedUserId]['Name']?></div>
+            <div class="monthName"><?=$monthArray[$selectedMonth]?></div>
+        </div>
+        <div class="form-group">
+            <label for="user-amount ">Amount </label>
+            <input type="Number" required="required" class="form-control" required="true" value="5000"
+                name="user-<?=$selectedUserId?>-month-<?=$selectedMonth?>-amount" placeholder="5000">
+        </div>
+        <div class="form-group">
+            <label for="late fine ">Late fine </label>
+            <input type="Number" required="required" class="form-control" required="true"
+                name="user-<?=$selectedUserId?>-month-<?=$selectedMonth?>-late-fine" placeholder="500">
+        </div>
+        <?php }}?>
+        <button type="submit" class="btn btn-primary" value="<?php echo $depositDetailsData['Deposite_ID']; ?>"
             name='Deposite_ID'>Verify</button>
-        <?php } ?>
+        <?php
+}?>
     </form>
 </div>
 
 <?php
-require_once('../footer.php'); 
+require_once '../footer.php';
 ?>
